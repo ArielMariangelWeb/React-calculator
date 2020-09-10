14afadb965d146c6ee48756f15a6fa4030fa3fe3
Ariel Mariangel <arielmariangelweb@gmail.com>
2020-09-10 00:16:40 -0300

    Se agrego carpeta 'docs'

---
+++ docs/precache-manifest.20d40b917a8012f81033e2004034de12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0508873dd4f57bb1286e6dc13378decd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "678a1afb4a3bce977580",
+    "url": "/static/css/main.654c96b1.chunk.css"
+  },
+  {
+    "revision": "67cf4ebdea0eeebd298b",
+    "url": "/static/js/2.c67ec3da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c67ec3da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678a1afb4a3bce977580",
+    "url": "/static/js/main.8ea22a07.chunk.js"
+  },
+  {
+    "revision": "01be7e6df4731114562b",
+    "url": "/static/js/runtime-main.76525902.js"
+  }
+]);
